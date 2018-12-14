f3f6a3163a00a5edbe46454477bbbcd358583091
Maria Shestakova <marijashestakova73@gmail.com>
2018-12-14 16:55:26 +0400

    add comparing defs

--- weather_yandex.py
+++ weather_yandex.py
@@ -30,8 +30,62 @@ class Example:
             print("RESPONSE:" + resp_str)
             # self.temp = str(resp_obj[0]['RealFeelTemperature']['Metric']['Value']) + resp_obj[0]['RealFeelTemperature']['Metric']['Unit']
             resp_obj = json.loads(resp_str)
-            print("RESP OBJ:")
-            print(resp_obj[0]['WeatherText'])
+            LocalObservationDateTime = resp_obj[0]['LocalObservationDateTime']
+
+            self.weather_text = resp_obj[0]['WeatherText'].lower()
+            self.temp = resp_obj[0]['Temperature']['Metric']['Value']
+            self.humidity = resp_obj[0]['RelativeHumidity']
+            LocalObservationDate = LocalObservationDateTime[:10]
+            LocalObservationTime = LocalObservationDateTime[11:19]
+
+            def compare_temp(self, temp):
+                if temp < -30.0:
+                    return 'очень холодно'
+                elif (temp >= -30.0) and (temp < -20.0):
+                    return 'морозно'
+                elif (temp >= -20.0) and (temp < -10.0):
+                    return 'холодно'
+                elif (temp >= -10.0) and (temp < 0.0):
+                    return 'прохладно'
+                elif (temp >= 0.0) and (temp < -0.0):
+                    return 'нормально'
+                elif (temp >= 10.0) and (temp < 20.0):
+                    return 'тепло'
+                elif (temp >= 20.0) and (temp < 30.0):
+                    return 'жарко'
+                elif temp >= 30.0:
+                    return 'очень жарко'
+
+            def compare_humidity(self, humidity):
+                if humidity == 0:
+                    return '0%'
+                elif (humidity > 0) and (humidity <= 33):
+                    return '20%'
+                elif (humidity > 33) and (humidity <= 66):
+                    return '50%'
+                elif (humidity > 66) and (humidity <= 100):
+                    return '80%'
+
+            def compare_conditions(self, text):
+                if 'солнечно' in text or 'ясно' in text:
+                    return 'тепло'
+                elif 'облачно' in text or 'пасмурно' in text or 'ветренно' in text or 'ветер' in text:
+                    return 'прохладно'
+                elif 'дождь' in text:
+                    return 'дождь'
+                elif 'ливень' in text:
+                    return 'ливень'
+                elif 'снег' in text or 'лёд' in text or 'лед' in text:
+                    return 'снег'
+                elif 'гроза' in text or 'гром' in text or 'гром' in text:
+                    return 'гроза'
+                else:
+                    return 'ok'
+
+            print(compare_temp(self, self.temp))
+            print(compare_humidity(self, self.humidity))
+            print(compare_conditions(self, self.weather_text))
+
 
         else:
             print("Error occured: ", er)
